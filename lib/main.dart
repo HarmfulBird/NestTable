adff551760b31ebea72b736ae4967d4373dca1ef
HarmlessBird <cocroper@gmail.com>
2025-06-12 13:32:31 +1200

    Updating REPO

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import '../firebase_options.dart';
 import 'package:provider/provider.dart';
 import 'Pages/tableview_simple.dart';
 import 'Pages/reservations.dart';
